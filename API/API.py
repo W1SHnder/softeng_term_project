5d2ec14523ea07899ef50e86a23f6fbe7d3dc55c
w1shnder <ben02taylor@gmail.com>
2024-02-25 09:16:24 -0500

    API

---
+++ API/API.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# Configuration for MariaDB connection
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Lucygirl321',
+    'database': 'MovieDB'
+}
+
+# Function to connect to MariaDB and execute queries
+def execute_query(query, params=()):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query, params)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+# API endpoint to fetch movies by title
+#URL: https://<domain>/movies?title=<title>
+@app.route('/movies')
+def get_movies_by_title():
+    title = request.args.get('title')
+    if title:
+        query = "SELECT * FROM Movie WHERE title LIKE %s"
+        movies = execute_query(query, (f'%{title}%',))
+        return jsonify(movies)
+    else:
+        return jsonify({'error': 'Title parameter is required.'}), 400
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
