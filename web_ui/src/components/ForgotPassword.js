a5d67a5890678b3a941901a20bd6bec5de724b1a
Noah Nielsen <njn04807@uga.edu>
2024-05-07 15:10:34 -0400

    login and registration working

--- web_ui/src/components/ForgotPassword.js
+++ web_ui/src/components/ForgotPassword.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 import './styles/ForgotPassword.css';
 
 const ForgotScreen = () => {
+    const [showCodeInput, setShowCodeInput] = useState(false);
+    const [buttonText, setButtonText] = useState('Send Code');
+    const [values, setValues] = useState({});
+    const navigate = useNavigate();
+
+    const handleSendCode = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.get(`http://127.0.0.1:8000/Verify/${values.email}`);
+            console.log(response);
+        } catch (error) {
+            console.error('Error fetching data:', error.response.data.message);
+        }
+        setShowCodeInput(true);
+        setButtonText('Submit');
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = {
+            "email": values.email,
+            "password": values.password,
+            "code": values.code
+        }
+        axios.post('http://127.0.0.1:8000/RecoverPassword/', data, { withCredentials: true })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setErrors({ error: 'Invalid email or password. Please try again.' });
+            });
+            navigate('/login');
+        setShowCodeInput(true);
+        setButtonText('Submit');
+    };
+
+    const handleInput = (event) => {
+        const { name, value } = event.target;
+        setValues(prevValues => ({ ...prevValues, [name]: value }));
+    };
+
     return (
         <div className="forgot-container">
             <h2>Recover</h2>
             <div className='underline'></div>
             <form action="server" method="post">
-                <input type="text" id="email" name="email" placeholder="Email" />
-                <button type="submit">Send Code</button>
+                <input type="text" id="email" name="email" placeholder="Email" value={values.email} onChange={handleInput}/>
+                <input type="password" id="password" name="password" placeholder='Password' value={values.password} onChange={handleInput}/>
+                {showCodeInput && (
+                    <input type="text" id="code" name="code" placeholder="Code" value={values.code} onChange={handleInput}/>
+                )}
+                <button type="button" onClick={showCodeInput ? handleSubmit : handleSendCode}>{buttonText}</button>
             </form>
         </div>
     );
--- web_ui/src/components/Login.js
+++ web_ui/src/components/Login.js
@@ -17,8 +17,9 @@ const LoginScreen = () => {
     }
 
     const handleInput = (event) => {
-        const { name, value} = event.target;
-        setValues(prevValues => ({ ...prevValues, [name]: value }));
+        const { name, value, type, checked } = event.target;
+        const inputValue = type === 'checkbox' ? checked : value;
+        setValues(prevValues => ({ ...prevValues, [name]: inputValue }));
     };
 
     const handleSubmit = async (event) => {
@@ -26,17 +27,18 @@ const LoginScreen = () => {
         const loginData = {
             "email": values.email,
             "password": values.password,
-            "long_session": "true"
+            "long_session": values.remember
         }
-
-        try {
-            const response = await axios.post('http://127.0.0.1:8000/Login/', loginData);
-            console.log('Response:', response.data);
-            navigate('/profile');
-        } catch (error) {
-            console.error('Error:', error.response.data.message);
-            setErrors({ error: error.response.data.message });
-        }
+    
+        axios.post('http://127.0.0.1:8000/Login/', loginData, {withCredentials:true})
+            .then(response => {
+                console.log(response.data);
+                navigate('/profile');
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setErrors({ error: 'Invalid email or password. Please try again.' });
+            });
     }
 
     return (
@@ -44,15 +46,15 @@ const LoginScreen = () => {
             <h2>Sign In</h2>
             <div className='underline'></div>
             <form action="server" method="post">
-                <input type="text" id="email" name="email" placeholder="Email" onChange={handleInput} value={values.email}/>
-                <div class="password-container">
+                <input type="text" id="email" name="email" placeholder="Email" onChange={handleInput} value={values.email} />
+                <div className="password-container">
                     <input type="password" id="password" name="password" placeholder="Password" onChange={handleInput} value={values.password} />
-                    <i class="eye-icon bi bi-eye-slash" id="togglePassword" onClick={() => togglePassword()}></i>
+                    <i className="eye-icon bi bi-eye-slash" id="togglePassword" onClick={() => togglePassword()}></i>
                 </div>
                 <button type="submit" onClick={handleSubmit}>Sign in</button>
                 <br></br>
                 <div className="remember">
-                    <input type="checkbox" name="remember" />
+                    <input type="checkbox" name="remember" value={values.remember}/>
                     <label htmlFor="remember">Remember me</label>
                 </div>
                 <div><Link to="/forgotPassword" id="forgot-password">Forgot Password?</Link></div>
