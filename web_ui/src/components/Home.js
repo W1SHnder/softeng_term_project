026bd92277d6bb0abb07e13ce2e23d9b4e057020
Noah Nielsen <njn04807@uga.edu>
2024-05-09 00:04:16 -0400

    login done

--- web_ui/src/components/Home.js
+++ web_ui/src/components/Home.js
@@ -6,6 +6,7 @@ import DatePicker from 'react-datepicker';
 import { useNavigate } from 'react-router-dom';
 import './styles/Home.css';
 import "react-datepicker/dist/react-datepicker.css";
+axios.defaults.withCredentials = true;
 
 /*
 import './assets/images/mpaa_G.png';
