a4ed492a6f636be831cfe2506f85309bd26c728e
w1shnder <ben02taylor@gmail.com>
2024-05-09 15:37:20 -0400

    README update

--- API/Unchained/Unchained/settings.py
+++ API/Unchained/Unchained/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'defnotphishing99@gmail.com'
-EMAIL_HOST_PASSWORD = 'qihcawhpzqmqdizp'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
